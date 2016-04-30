a1b2cab34e81e79d92a506d313c100f575fcf42d
septor92 <ivanx_92@hotmail.com>
2016-04-30 02:59:23 +0000

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,99 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope,$ionicPopup, $timeout,$cordovaSQLite) {
+    
+    $scope.showAlert = function() {
+     var alertPopup = $ionicPopup.alert({
+       title: 'reserva',
+       template: 'Datos guardados'
+     });
+    }
+    
+    $scope.guardar = function(persona){
+        
+        $cordovaSQLite.execute(db, 'INSERT INTO reserva (origen,destino,fechaini,fecharegre,personas,costo) VALUES (?,?,?,?,?,?)', [persona.origen,persona.destino,persona.fechaini,persona.fecharegre,persona.personas,persona.costo])
+        .then(function(result) {
+            $scope.statusMessage = "Registro guardado!";
+        }, function(error) {
+            $scope.statusMessage = "Error al guardar: " + error.message;
+        })
+        
+        /*
+        console.log("Nombre: "+persona.nombre);
+        console.log("Apellido: "+persona.apellido);
+        console.log("Telefono: "+persona.telefono);
+        console.log("Email: "+persona.email);
+        */
+    }
+    
+})
+
+.controller('ChatsCtrl', function($scope, Chats,$cordovaSQLite) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  //$scope.chats = [];
+  
+  /*
+  $cordovaSQLite.execute(db, 'SELECT * FROM agenda ORDER BY id DESC')
+       .then(
+          function(result) {
+             if (result.rows.length > 0) {
+                      for(var i = 0; i < result.rows.length; i++)
+                      { 
+                        $scope.chats.push({"nombre":result.rows.item(i).nombre,
+                                    "apellido":result.rows.item(i).apellido,
+                                    "telefono":result.rows.item(i).telefono,
+                                    "email":result.rows.item(i).email});
+                      }
+                    }
+                },
+                function(error) {
+                    $scope.statusMessage = "Error on loading: " + error.message;
+                }
+        );
+  */
+
+  $scope.getAll = function()
+  {
+      $scope.chats = Chats.all();
+  };
+  
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats,$cordovaSQLite) {
+  
+  //$scope.chat = Chats.get($stateParams.chatId);
+  $scope.persona = Chats.get($stateParams.chatId);
+  
+  $scope.guardar = function(persona){
+        
+        $cordovaSQLite.execute(db, 'UPDATE reserva set origen=?,destino=?,fechaini=?,fecharegre=?,personas=?,costo=? where id = ?', [persona.origen,persona.destino,persona.fechaini,persona.fecharegre,persona.personas,persona.costo,persona.id])
+        .then(function(result) {
+            $scope.statusMessage = "Registro guardado!";
+        }, function(error) {
+            $scope.statusMessage = "Error al guardar: " + error.message;
+        })
+        
+        console.log("ORIGEN: "+persona.origen);
+        console.log("ID: "+persona.id);
+    }
+  
+    
+  
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ www/js/services.js
@@ -0,0 +1,125 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function($cordovaSQLite) {
+  // Might use a resource here that returns a JSON arrays
+
+  // Some fake testing data
+  var chats = [];
+  var chatObj = "0";
+
+  
+  return {
+    all: function() {
+
+      chats = [];
+
+      $cordovaSQLite.execute(db, 'SELECT * FROM reserva ORDER BY id DESC')
+       .then(
+          function(result) {
+             if (result.rows.length > 0) {
+                      for(var i = 0; i < result.rows.length; i++)
+                      { 
+                        chats.push({"id":result.rows.item(i).id,
+                                    "origen":result.rows.item(i).nombre,
+                                    "destino":result.rows.item(i).apellido,
+                                    "fechaini":result.rows.item(i).telefono,
+                                    "fecharegre":result.rows.item(i).email,
+                                    "personas":result.rows.item(i).email,
+                                    "costo":result.rows.item(i).email});
+                      }
+                    }
+                },
+                function(error) {
+                    statusMessage = "Error on loading: " + error.message;
+                }
+        );
+
+      return chats;
+    },
+    remove: function(chat) {
+      $cordovaSQLite.execute(db, 'DELETE FROM reserva where id = ?',[chat.id])
+      .then(function(result){
+          statusMessage = "Borrado";
+          chats.splice(chats.indexOf(chat), 1);
+      },
+      function(error){
+          statusMessage = "Error: " + error.message;
+      });
+    },
+    
+    get: function(chatId) {
+
+        chats = [];
+        
+        $cordovaSQLite.execute(db, 'SELECT * FROM reserva where id = ?',[chatId])
+       .then(
+          function(result) {
+               
+             if (result.rows.length > 0) {
+                        chats.push({"id":result.rows.item(0).id,
+                                     "origen":result.rows.item(i).nombre,
+                                    "destino":result.rows.item(i).apellido,
+                                    "fechaini":result.rows.item(i).telefono,
+                                    "fecharegre":result.rows.item(i).email,
+                                    "personas":result.rows.item(i).email,
+                                    "costo":result.rows.item(i).email});                 
+                
+                    }
+                    
+                },
+                function(error) {
+                    statusMessage = "Error on loading: " + error.message;
+                }
+        );
+
+      return chats;
+
+    }
+
+  };
+
+
+  /*
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };*/
+});
